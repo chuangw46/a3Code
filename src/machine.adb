19dc26d7a452b55695cf52fdafaa9a341404cc0c
Chuang Wang <chuangw@student.unimelb.edu.au>
2019-05-30 20:27:10 +1000

    chuangw2

--- src/machine.adb
+++ src/machine.adb
@@ -9,6 +9,7 @@ package body Machine with SPARK_Mode is
    -- data values are 32-bit integers
    -- this is the type of words used in the virtual machine
    type DataVal is range -(2**31) .. +(2**31 - 1);
+   -- define a type on bigger range for the easy of range and overflow checkings
    type GalaxyVal is range -(2**62)..+(2**62);
       
    -- the registers
@@ -20,74 +21,218 @@ package body Machine with SPARK_Mode is
    -- the program counter
    PC : ProgramCounter := ProgramCounter'First;
       
-   procedure IncPC(Ret : out ReturnCode; Offs : in Offset) is
+   procedure IncPC(Ret : out ReturnCode; Offs : in Offset)
+   is
+      TempVal : GalaxyVal;
    begin
-      PC := ProgramCounter(Integer(PC) + Integer(Offs));
-      Ret := Success;
+      -- validate the value in Offs
+      if Offset'First > Offs or Offs > Offset'Last then
+          Ret := IllegalProgram;
+      -- increment pc by 0 is an infinite loop, so return CyclesExhausted immediately
+      elsif Offs = 0 then
+          Ret := CyclesExhausted;
+      else
+        TempVal := GalaxyVal(PC) + GalaxyVal(Offs);
+        if TempVal >= 1 and TempVal <= 65536 then
+          PC := ProgramCounter(Integer(TempVal));
+          Ret := Success;
+        else
+          Ret := IllegalProgram;
+        end if;
+      end if;
    end IncPC;
    
    procedure DoAdd(Rd : in Reg; 
                    Rs1 : in Reg; 
                    Rs2 : in Reg;
-                   Ret : out ReturnCode) is
+                   Ret : out ReturnCode) 
+   is
+      TempVal : GalaxyVal;
    begin
-      Regs(Rd) := Regs(Rs1) + Regs(Rs2);
-      Ret := Success;
+      -- validate the value in Regs(Rd)
+      if DataVal'First > Regs(Rd) or Regs(Rd) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs1)
+      elsif DataVal'First > Regs(Rs1) or Regs(Rs1) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs2)
+      elsif DataVal'First > Regs(Rs2) or Regs(Rs2) > DataVal'Last then
+          Ret := IllegalProgram;
+      else
+        -- if pass all validations, compute the sum
+        TempVal := GalaxyVal(Regs(Rs1)) + GalaxyVal(Regs(Rs2));
+        -- validate their sum value
+        if TempVal >= -(2**31) and TempVal <= (2**31 - 1) then
+          Regs(Rd) := DataVal(TempVal);
+          Ret := Success;
+        else
+          Ret := IllegalProgram;
+        end if;
+      end if;
    end DoAdd;
    
    procedure DoSub(Rd : in Reg; 
                    Rs1 : in Reg; 
                    Rs2 : in Reg;
-                   Ret : out ReturnCode) is
+                   Ret : out ReturnCode) 
+   is
+      TempVal : GalaxyVal;
    begin
-      Regs(Rd) := Regs(Rs1) - Regs(Rs2);
-      Ret := Success;
+      -- validate the value in Regs(Rd)
+      if DataVal'First > Regs(Rd) or Regs(Rd) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs1)
+      elsif DataVal'First > Regs(Rs1) or Regs(Rs1) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs2)
+      elsif DataVal'First > Regs(Rs2) or Regs(Rs2) > DataVal'Last then
+          Ret := IllegalProgram;
+      else
+        -- if pass all validations, compute the difference
+        TempVal := GalaxyVal(Regs(Rs1)) - GalaxyVal(Regs(Rs2));
+        -- validate their difference
+        if TempVal >= -(2**31) and TempVal <= (2**31 - 1) then
+          Regs(Rd) := DataVal(TempVal);
+          Ret := Success;
+        else
+          Ret := IllegalProgram;
+        end if;
+      end if;
    end DoSub;
    
    procedure DoMul(Rd : in Reg; 
                    Rs1 : in Reg; 
                    Rs2 : in Reg;
-                   Ret : out ReturnCode) is
+                   Ret : out ReturnCode) 
+   is
+      TempVal : GalaxyVal;
    begin
-      Regs(Rd) := Regs(Rs1) * Regs(Rs2);
-      Ret := Success;
+      -- validate the value in Regs(Rd)
+      if DataVal'First > Regs(Rd) or Regs(Rd) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs1)
+      elsif DataVal'First > Regs(Rs1) or Regs(Rs1) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs2)
+      elsif DataVal'First > Regs(Rs2) or Regs(Rs2) > DataVal'Last then
+          Ret := IllegalProgram;
+      else
+        -- if pass all validation, compute the product
+        TempVal := GalaxyVal(Regs(Rs1)) * GalaxyVal(Regs(Rs2));
+        -- validate their product
+        if TempVal >= -(2**31) and TempVal <= (2**31 - 1) then
+          Regs(Rd) := DataVal(TempVal);
+          Ret := Success;
+        else
+          Ret := IllegalProgram;
+        end if;
+      end if;
    end DoMul;
    
    procedure DoDiv(Rd : in Reg; 
                    Rs1 : in Reg; 
                    Rs2 : in Reg;
-                   Ret : out ReturnCode) is
+                   Ret : out ReturnCode) 
+   is
+      TempVal : GalaxyVal;
    begin
-      Regs(Rd) := Regs(Rs1) / Regs(Rs2);
-      Ret := Success;
+      -- validate if divisor is zero
+      if Regs(Rs2) = 0 then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rd)
+      elsif DataVal'First > Regs(Rd) or Regs(Rd) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs1)
+      elsif DataVal'First > Regs(Rs1) or Regs(Rs1) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs2)
+      elsif DataVal'First > Regs(Rs2) or Regs(Rs2) > DataVal'Last then
+          Ret := IllegalProgram;
+      else
+        -- if pass all validations, compute the quotient
+        TempVal := GalaxyVal(Regs(Rs1)) / GalaxyVal(Regs(Rs2));
+        -- validate their quotient
+        if TempVal >= -(2**31) and TempVal <= (2**31 - 1) then
+          Regs(Rd) := DataVal(TempVal);
+          Ret := Success;
+        else
+          Ret := IllegalProgram;
+        end if;
+      end if;
    end DoDiv;
    
    procedure DoLdr(Rd : in Reg; 
                    Rs : in Reg; 
                    Offs : in Offset;
-                   Ret : out ReturnCode) is
-      A : Addr := Addr(Regs(Rs) + DataVal(Offs));
+                   Ret : out ReturnCode) 
+   is
+      TempVal : GalaxyVal;
    begin
-      Regs(Rd) := Memory(A);
-      Ret := Success;
+      -- validate the value in Regs(Rd)
+      if DataVal'First > Regs(Rd) or  Regs(Rd) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rs)
+      elsif DataVal'First > Regs(Rs) or  Regs(Rs) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Offs
+      elsif Offset'First > Offs or Offs > Offset'Last then
+          Ret := IllegalProgram;
+      else
+        -- if individual reg or offs are valid, then compute the address
+        TempVal := GalaxyVal(Regs(Rs)) + GalaxyVal(Offs);
+        -- validate the computed address
+        if TempVal in 0 .. 65535 then
+            Regs(Rd) := Memory(Addr(TempVal));
+            Ret := Success;
+        else
+            Ret := IllegalProgram;
+        end if;
+      end if;
    end DoLdr;
    
    procedure DoStr(Ra : in Reg;
                    Offs : in Offset;
                    Rb : in Reg;
-                   Ret : out ReturnCode) is
-      A : Addr := Addr(Regs(Ra) + DataVal(Offs));   
+                   Ret : out ReturnCode) 
+   is
+      TempVal : GalaxyVal;
    begin
-      Memory(A) := Regs(Rb);
-      Ret := Success;
+      -- validate the value in Regs(Ra)
+      if DataVal'First > Regs(Ra) or  Regs(Ra) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Regs(Rb)
+      elsif DataVal'First > Regs(Rb) or  Regs(Rb) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Offs
+      elsif Offset'First > Offs or Offs > Offset'Last then
+          Ret := IllegalProgram;
+      else
+        -- if individual reg or offs are valid, then compute the address
+        TempVal := GalaxyVal(Regs(Ra)) + GalaxyVal(Offs);
+        -- validate the computed address
+        if TempVal in 0 .. 65535 then
+            Memory(Addr(TempVal)) := Regs(Rb);
+            Ret := Success;
+        else
+            Ret := IllegalProgram;
+        end if;
+      end if;
    end DoStr;
    
    procedure DoMov(Rd : in Reg;
                    Offs : in Offset;
                    Ret : out ReturnCode) is
    begin
-      Regs(Rd) := DataVal(Offs);
-      Ret := Success;
+      -- validate the value in Regs(Rd)
+      if DataVal'First > Regs(Rd) or  Regs(Rd) > DataVal'Last then
+          Ret := IllegalProgram;
+      -- validate the value in Offs
+      elsif Offset'First > Offs or Offs > Offset'Last then
+          Ret := IllegalProgram;
+      else
+          Regs(Rd) := DataVal(Offs);
+          Ret := Success;
+      end if;
    end DoMov;
    
    procedure ExecuteProgram(Prog : in Program;
@@ -112,37 +257,78 @@ package body Machine with SPARK_Mode is
          case Inst.Op is
             when ADD =>
                DoAdd(Inst.AddRd,Inst.AddRs1,Inst.AddRs2,Ret);
+                -- terminate the loop immediately if Ret is not Success
+                if Ret /= Success then
+                    return;
+                end if;
                IncPC(Ret,1);
             when SUB =>
                DoSub(Inst.SubRd,Inst.SubRs1,Inst.SubRs2,Ret);
+               -- terminate the loop immediately if Ret is not Success
+                if Ret /= Success then
+                    return;
+                end if;
                IncPC(Ret,1);
             when MUL =>
                DoMul(Inst.MulRd,Inst.MulRs1,Inst.MulRs2,Ret);
+               -- terminate the loop immediately if Ret is not Success
+                if Ret /= Success then
+                    return;
+                end if;
                IncPC(Ret,1);
             when DIV =>
                DoDiv(Inst.DivRd,Inst.DivRs1,Inst.DivRs2,Ret);
+               -- terminate the loop immediately if Ret is not Success
+                if Ret /= Success then
+                    return;
+                end if;
                IncPC(Ret,1);
             when LDR =>
                DoLdr(Inst.LdrRd,Inst.LdrRs,Inst.LdrOffs,Ret);
+               -- terminate the loop immediately if Ret is not Success
+                if Ret /= Success then
+                    return;
+                end if;
                IncPC(Ret,1);
             when STR =>
                DoStr(Inst.StrRa,Inst.StrOffs,Inst.StrRb,Ret);
+               -- terminate the loop immediately if Ret is not Success
+                if Ret /= Success then
+                    return;
+                end if;
                IncPC(Ret,1);
             when MOV =>
                DoMov(Inst.MovRd,Inst.MovOffs,Ret);
+               -- terminate the loop immediately if Ret is not Success
+                if Ret /= Success then
+                    return;
+                end if;
                IncPC(Ret,1);
             when Instruction.RET =>
-               Result := Integer(Regs(Inst.RetRs));
-               Ret := Success;
-               return;
+                -- validate the value in Regs(Inst.RetRs)
+                -- terminate the loop immediately if invalid             
+                if DataVal'First > Regs(Inst.RetRs) or Regs(Inst.RetRs) > DataVal'Last then
+                    Ret := IllegalProgram;
+                else
+                    -- execute Ret
+                    Result := Integer(Regs(Inst.RetRs));
+                    Ret := Success;
+                end if;
+                return;
             when JMP =>
                IncPC(Ret,Inst.JmpOffs);
             when JZ =>
-               if Regs(Inst.JzRa) = 0 then
-                  IncPC(Ret,Inst.JzOffs);
-               else
-                  IncPc(Ret,1);
-               end if;
+                -- validate the value in Regs(Inst.JzRa)
+                if DataVal'First > Regs(Inst.JzRa) or Regs(Inst.JzRa) > DataVal'Last then
+                    Ret := IllegalProgram;
+                    return;
+                end if;
+                -- execute JZ
+                if Regs(Inst.JzRa) = 0 then
+                   IncPC(Ret,Inst.JzOffs);
+                else
+                   IncPc(Ret,1);
+                end if;
             when NOP =>
                IncPC(Ret,1);
          end case;
@@ -161,12 +347,21 @@ package body Machine with SPARK_Mode is
         Inst : Instr;
         TempVal: GalaxyVal;
         -- the dummy registers for this function only
-        DummyRegs : array (Reg) of DataVal := (others => 0);
+        DummyRegs : array (Reg) of DataVal;
         -- the dummy memory for this function only
-        DummyMemory : array (Addr) of DataVal := (others => 0);
+        DummyMemory : array (Addr) of DataVal;
     begin
+        -- replicate Regs array
+        for I in reg loop
+             DummyRegs(I) := Regs(I);   
+        end loop;
+
+        -- replicate Memory array
+        for I in Addr loop
+             DummyMemory(I) := Memory(I);   
+        end loop;
+
         while (CycleCount < Cycles) loop
-            
             if CycleCount in 1 .. MAX_PROGRAM_LENGTH then 
               -- get next instruction if CycleCount is valid
               Inst := Prog(ProgramCounter(CycleCount));
@@ -265,15 +460,15 @@ package body Machine with SPARK_Mode is
 
                 when LDR =>
                     -- validate the value in LdrRd
-                    if DataVal'First > Regs(Inst.LdrRd) or  Regs(Inst.LdrRd) > DataVal'Last then
+                    if DataVal'First > Regs(Inst.LdrRd) or Regs(Inst.LdrRd) > DataVal'Last then
                         return True;
                     end if;
                     -- validate the value in LdrRs
-                    if DataVal'First > Regs(Inst.LdrRs) or  Regs(Inst.LdrRs) > DataVal'Last then
+                    if DataVal'First > Regs(Inst.LdrRs) or Regs(Inst.LdrRs) > DataVal'Last then
                         return True;
                     end if;
                     -- validate the value in LdrOffs
-                    if Offset'First > Inst.LdrOffs or  Inst.LdrOffs > Offset'Last then
+                    if Offset'First > Inst.LdrOffs or Inst.LdrOffs > Offset'Last then
                         return True;
                     end if;
                     -- if individual reg or offs are valid, then compute address
@@ -295,15 +490,15 @@ package body Machine with SPARK_Mode is
 
                 when STR =>
                     -- validate the value in StrRa
-                    if DataVal'First > Regs(Inst.StrRa) or  Regs(Inst.StrRa) > DataVal'Last then
+                    if DataVal'First > Regs(Inst.StrRa) or Regs(Inst.StrRa) > DataVal'Last then
                         return True;
                     end if;
                     -- validate the value in StrRb
-                    if DataVal'First > Regs(Inst.StrRb) or  Regs(Inst.StrRb) > DataVal'Last then
+                    if DataVal'First > Regs(Inst.StrRb) or Regs(Inst.StrRb) > DataVal'Last then
                         return True;
                     end if;
                     -- validate the value in StrOffs
-                    if Offset'First > Inst.StrOffs or  Inst.StrOffs > Offset'Last then
+                    if Offset'First > Inst.StrOffs or Inst.StrOffs > Offset'Last then
                         return True;
                     end if;
 
@@ -320,11 +515,11 @@ package body Machine with SPARK_Mode is
 
                 when MOV =>
                     -- validate the value in MovRd                
-                    if DataVal'First > Regs(Inst.MovRd) or  Regs(Inst.MovRd) > DataVal'Last then
+                    if DataVal'First > Regs(Inst.MovRd) or Regs(Inst.MovRd) > DataVal'Last then
                         return True;
                     end if;
                     -- validate MovOffs                
-                    if Offset'First > Inst.MovOffs or  Inst.MovOffs > Offset'Last then
+                    if Offset'First > Inst.MovOffs or Inst.MovOffs > Offset'Last then
                         return True;
                     end if;
 
@@ -346,7 +541,7 @@ package body Machine with SPARK_Mode is
                         return True;
                     end if;
                     -- check if value of JmpOffs is out of range
-                    if Offset'First > Inst.JmpOffs or  Inst.JmpOffs > Offset'Last then
+                    if Offset'First > Inst.JmpOffs or Inst.JmpOffs > Offset'Last then
                         return True;
                     end if;  
                     -- check if JMP intends to alter the PC to become out of range
@@ -361,7 +556,7 @@ package body Machine with SPARK_Mode is
                         return True;
                     end if;
                     -- check if value of JzOffs is out of range
-                    if Offset'First > Inst.JzOffs or  Inst.JzOffs > Offset'Last then
+                    if Offset'First > Inst.JzOffs or Inst.JzOffs > Offset'Last then
                         return True;
                     end if; 
                     if Regs(Inst.JzRa) = 0 then
